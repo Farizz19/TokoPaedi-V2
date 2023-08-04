557d2099e443ed5fecbe8151072e1fb7dde0cfb1
BjorRiz <mfarizalhaq1903@gmail.com>
2023-08-04 08:17:43 +0700

    SHOP

--- FrontEnd/src/components/Part/Navbar.jsx
+++ FrontEnd/src/components/Part/Navbar.jsx
@@ -5,10 +5,10 @@ function Navbar() {
     <nav class="navbar col-lg-12 col-12 p-0 fixed-top d-flex flex-row">
       <div class="text-center navbar-brand-wrapper d-flex align-items-center justify-content-center">
         <a class="navbar-brand brand-logo mr-5" href="index.html">
-          <img src="./public/assets/images/logo.svg" class="mr-2" alt="logo" />
+          <img src="./public/assets/images/tokopaedi.png" class="mr-2" alt="logo" />
         </a>
         <a class="navbar-brand brand-logo-mini" href="index.html">
-          <img src="./public/assets/images/logo-mini.svg" alt="logo" />
+          <img src="./public/assets/images/tokopaedi.png" alt="logo" />
         </a>
       </div>
       <div class="navbar-menu-wrapper d-flex align-items-center justify-content-end">
